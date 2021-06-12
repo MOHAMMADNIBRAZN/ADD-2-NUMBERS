34f00c8c52a9667c6aa6f5cc6353b3eb1c348598
MOHAMMADNIBRAZN <85448180+MOHAMMADNIBRAZN@users.noreply.github.com>
2021-06-12 16:11:46 +0530

    Add files via upload

---
+++ ADDTWONUMBERS.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class ADDTWONUMBERS {
+
+	public static void main(String[] args) {
+		int number1 = 1000, number2 = 150, sum;
+		sum = number1 + number2;
+		System.out.println("add of this numbers:"+sum);
+		
+
+	}
+
+}
